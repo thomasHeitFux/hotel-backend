6d17547eec886e8c8c113e44c00e9ba487761e85
thomasHeitFux <thomas41392@gmail.com>
2025-03-23 06:11:24 +0000

    cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
-const URL_LOCAL = "https://hotel-frontend-mu.vercel.app"
-// const { CORS_URL, PRODUCTION } = process.env;
+// En producción, usamos la URL de tu frontend desplegado en Vercel
+const URL_PRODUCTION = "https://hotel-frontend-mu.vercel.app"; // URL de tu frontend en Vercel
 
 require("./db.js");
 
@@ -13,26 +13,29 @@ const server = express();
 
 server.name = "API";
 
+// Middleware de CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", URL_PRODUCTION); // Permite solicitudes desde la URL de producción
+  res.header("Access-Control-Allow-Credentials", "true"); // Permite el manejo de credenciales (cookies, etc.)
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
+
+  // Si la solicitud es OPTIONS, respondemos inmediatamente
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", URL_LOCAL  ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
-  next();
-});
-
 server.use("/", routes);
 
-// Error catching endware.
+// Middleware de manejo de errores
 server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
